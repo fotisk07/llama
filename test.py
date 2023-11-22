d62a90a24469e8ac1893ea66b587332e38c5058a
Fotis <fotiskapotos@gmail.com>
2023-11-22 21:23:23 +0100

    Generating function

--- test.py
+++ test.py
@@ -28,4 +28,11 @@ model = Baseline(vocab_size)
 # Train
 context_window = 10
 batch_size = 32
+print("Training...")
 model = train_model(model, train, lr=1e-3, epochs=10, batch_size=batch_size, context_window=context_window)
+
+
+# Generate
+print("\nGenerating...")
+idx = torch.zeros((1,1), dtype=torch.long)
+print(decode(model.generate(idx, 100)[0].tolist(), itos))
